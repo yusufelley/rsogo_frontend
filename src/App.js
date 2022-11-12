0112f904d2dbdd12fc6130e716462fa13ef3768f
yos497 <75061482+yos497@users.noreply.github.com>
2022-11-12 18:06:52 -0500

    Added flyer2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import HomeCard from './components/HomeCard/HomeCard'
 import msa from './assets/msa.png'
 import msaFlyer from './assets/flyer.jpg'
+import testFlyer from './assets/flyer2.png'
 import  EventCard from './components/EventCard'
 import { Home } from './screens/Home';
 
