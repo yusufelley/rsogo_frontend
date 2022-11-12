a587a75bfcc06490bf8bd65a9be14a059d7b0a67
mounirkanane <71102660+MounirKanane@users.noreply.github.com>
2022-11-12 14:04:49 -0500

    Update EventContainer.js

--- src/components/EventContainer.js
+++ src/components/EventContainer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function EventContainer(props) {
   return (
-    <div style={containerStyle} className="shadow">
+    <div style={containerStyle} >
         <div>
             Welcome Back
         </div>
@@ -14,11 +14,8 @@ function EventContainer(props) {
 const containerStyle = { 
     width: "60vw",
     height: "40vh",
-    background: "Cyan",
-    borderRadius: 15,
-    position: "relative",
-    left: '5wv',
-    top: '5vh',
-    
+    background: "golden",
+    borderRadius: 15
 }
+
 export default EventContainer
