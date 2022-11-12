20aec47e9794d94500e15b7ae2d26275499b5d03
evan175 <ashraful.e.hoque@gmail.com>
2022-11-12 18:51:49 -0500

    update

--- src/App.js
+++ src/App.js
@@ -10,9 +10,7 @@ import { Home } from './screens/Home';
 
 function App() {
   return (
-    <div className="App" style={{backgroundColor:"#f9fafbff"}}>
-      <EventCard img = {testFlyer}></EventCard>
-    </div>
+    <Home />
   );
 }
 
