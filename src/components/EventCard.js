bb074034778c8c82a7f838b5c85a7554cb704a1f
mounirkanane <71102660+MounirKanane@users.noreply.github.com>
2022-11-12 15:30:00 -0500

    Event Details bar

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,24 +1,69 @@
 import React from 'react'
+import {AiOutlineClockCircle} from "react-icons/ai"
+import {FaRegCalendarAlt} from "react-icons/fa"
+import {MdOutlineLocationOn} from "react-icons/md"
 
-function EventContainer(props) {
+const ICON_SIZE = 15
+
+
+function EventCard(props) {
   return (
     <div style={containerStyle} className="shadow">
-        <div>
-            Welcome Back
+        <div style={title}>
+          Welcome Back!
         </div>
-        EventContainer
+        <div style={eventDetails}>
+          <div style={iconContainer}>
+          <AiOutlineClockCircle size={ICON_SIZE}/>
+          <p style={iconText}>7:30PM</p>
+          </div>
+          <div style={iconContainer}>
+          <FaRegCalendarAlt size={13}/>
+          <p style={iconText}>10/22/2022</p>
+          </div>
+          <div style={iconContainer}>
+          <MdOutlineLocationOn size={ICON_SIZE}/>
+          <p style={iconText}>Campus Center</p>
+          </div>
+        </div>
     </div>
   )
 }
 
-const title = {
-    
-}
+// export const icons = ({text,icon}) => {
+//   return (
+//     <div>
+//       <div style={iconContainer}>
+//         <div>{icon}</div>
+//         <p style={iconText}>{text}</p>
+//       </div>  
+//     </div>
+//   )
+// }
+
 const containerStyle = { 
-    width: "60vw",
-    height: "40vh",
-    background: "golden",
-    borderRadius: 15
+  width: "60vw",
+  height: "40vh",
+  background: "white",
+  borderRadius: 15
 }
 
-export default EventContainer
+const title = {
+  fontSize: 15,
+  marginLeft: 5
+}
+
+const eventDetails = {
+  display: "flex",
+  fontSize: 10,
+  justifyContent: "center",
+}
+const iconContainer = {
+  display: "flex",
+  fontSize: 10,
+  marginLeft: 4
+}
+const iconText = {
+  marginLeft: 2
+}
+export default EventCard
