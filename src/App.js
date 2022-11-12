09698a0ea68ba21e6862c05ab30cc8b56b4913dc
yos497 <75061482+yos497@users.noreply.github.com>
2022-11-12 16:01:29 -0500

    Bottom bar initial progress

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Home } from './screens/Home';
 function App() {
   return (
     <div className="App">
-      <EventContainer/>
+      <EventCard/>
       <Home/>
     </div>
   );
--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,24 +1,39 @@
 import React from 'react'
-
-function EventContainer(props) {
+import { RiQuestionnaireLine } from 'react-icons/ri'
+import { BsCalendarPlus } from 'react-icons/bs'
+import Button from 'react-bootstrap/Button'
+ 
+function EventCard(props) {
   return (
     <div style={containerStyle} className="shadow">
         <div>
             Welcome Back
         </div>
-        EventContainer
+        <div align="center" style={{display:"flex",width:'100%',justifyContent: 'space-around'}}>
+          <RiQuestionnaireLine size={'40px'}></RiQuestionnaireLine>
+          <div>
+            <BsCalendarPlus size={'35px'}></BsCalendarPlus>
+            <Button className="btn-dark" style = {buttonStyle}>RSVP</Button>
+          </div>
+        </div>
     </div>
   )
 }
 
-const title = {
-    
-}
-const containerStyle = { 
+const containerStyle = {
     width: "60vw",
     height: "40vh",
-    background: "golden",
-    borderRadius: 15
+    background: "White",
+    borderRadius: 15,
+    position: "relative",
+    left: '5wv',
+    top: '5vh',
 }
 
-export default EventContainer
+const buttonStyle = {
+  color: "White",
+  fontWeight: "bold",
+  borderRadius: '18px 5px 18px 18px'
+}
+
+export default EventCard
