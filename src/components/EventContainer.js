42d87326c61af13fb9d49a53b33fffb89fcfebea
yos497 <75061482+yos497@users.noreply.github.com>
2022-11-12 13:58:16 -0500

    Added shadow

--- src/components/EventContainer.js
+++ src/components/EventContainer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function EventContainer(props) {
   return (
-    <div style={containerStyle} className="shadow-3">
+    <div style={containerStyle} className="shadow">
         <div>
             Welcome Back
         </div>
@@ -14,8 +14,11 @@ function EventContainer(props) {
 const containerStyle = { 
     width: "60vw",
     height: "40vh",
-    background: "golden",
-    borderRadius: 15
+    background: "Cyan",
+    borderRadius: 15,
+    position: "relative",
+    left: '5wv',
+    top: '5vh',
     
 }
 export default EventContainer
