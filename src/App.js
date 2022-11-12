171f724ad2c333c13bfc809321adcf9de6f499ff
mounirkanane <71102660+MounirKanane@users.noreply.github.com>
2022-11-12 13:47:55 -0500

    EventContainer Progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import  EventContainer from './components/EventContainer'
 import { Home } from './screens/Home';
 
 function App() {
   return (
     <div className="App">
-
+      <EventContainer/>
       <Home/>
       
     </div>
--- src/components/EventContainer.js
+++ src/components/EventContainer.js
@@ -2,8 +2,20 @@ import React from 'react'
 
 function EventContainer(props) {
   return (
-    <div>EventContainer</div>
+    <div style={containerStyle} className="shadow-3">
+        <div>
+            Welcome Back
+        </div>
+        EventContainer
+    </div>
   )
 }
 
+const containerStyle = { 
+    width: "60vw",
+    height: "40vh",
+    background: "golden",
+    borderRadius: 15
+    
+}
 export default EventContainer
