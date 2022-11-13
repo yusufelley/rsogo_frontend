dd82926b24841188ffc7e92e8a0c1b607a1e04e4
ZMTakriti <71720717+ZMTakriti@users.noreply.github.com>
2022-11-13 00:20:22 -0500

    Fix RSOIcon sizes

---
+++ src/components/SideBar/SideBar.js
@@ -0,0 +1,83 @@
+import { AiFillHome } from "react-icons/ai"
+import React from 'react';
+import './SideBar'
+import RSOIcon from "../RSOIcon/RSOIcon"
+import msa from '../../assets/msa.png'
+
+const SideBar = ({rsoData}) => {
+    
+    // state = {
+    //     touched: false
+    // }
+
+    // const toggleTouched = () => {
+    //     this.setState( prevState => ({
+    //         touched: !prevState.touched
+    //     }));
+    // }
+      
+    // const handleMouseUp = () => {
+    //     // Handle smooth animation when clicking without holding
+    //     setTimeout( () => {
+    //         this.setState({ touched: false });
+    //     }, 150);
+    // }
+    
+    return(
+        
+        <div style = {sidebarStyle}>
+            {/* {/* <button style = {homeButton}>
+                <AiFillHome style = {iconStyle}/>
+            </button> */}
+            <button style = {homeButton}>
+                <AiFillHome style = {iconStyle}/>
+            </button>
+            
+            {/* <button
+                className={className}
+                onMouseDown={this.toggleTouched}
+                onMouseUp={this.handleMouseUp}
+                >
+                <AiFillHome style = {iconStyle}/>
+                
+            </button> */}
+            <hr
+                style={{
+                    background: 'white',
+                    color: 'white',
+                    borderColor: 'white',
+                    height: '3px',
+                    width: '10vw',
+                    transparency: "0"
+                }}
+            />
+            {rsoData.map((rso) => <div style={{marginBottom: "10px"}}><RSOIcon img={rso.img}/></div>)}
+            {/* <RSOIcon img = {msa}/> */}
+        </div>
+    )
+}
+
+const sidebarStyle = {
+    display: "flex",
+    height: "100vh",
+    width: "15vw",
+    background: 'maroon',
+    flexDirection: "column",
+    alignItems: "center"
+}
+
+const iconStyle = {
+    height: "4vh",
+    width: "11vw"
+}
+
+const homeButton = {
+    marginTop: "1vh",
+    height: "6vh",
+    width: "15vw",
+    color: "white",
+    backgroundColor: "maroon",
+    borderColor: "transparent"
+}
+
+export default SideBar;
