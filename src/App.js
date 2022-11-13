abc497c48f1cd592c4cfa8c9b1fec32689c0ea28
Yusuf Elley <yelley@vl965-172-31-196-3.wireless.umass.edu>
2022-11-13 05:49:51 -0500

    homescreen chronological rendering

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import HomeCard from "./components/HomeCard/HomeCard";
 
 import msa from "./assets/msa.png";
-import smash from "./assets/umass-smashbros.png"
-import hockey from "./assets/umass-hockey.jpg"
-import acm from "./assets/umass-acm.png"
-import archery from "./assets/umass-archery.jpg"
+import smash from "./assets/umass-smashbros.png";
+import hockey from "./assets/umass-hockey.jpg";
+import acm from "./assets/umass-acm.png";
+import archery from "./assets/umass-archery.jpg";
 
 import msaFlyer from "./assets/flyer.jpg";
 import testFlyer from "./assets/flyer2.png";
@@ -18,15 +18,12 @@ import React, { useState } from "react";
 import OneRSO from "./screens/OneRSO";
 import { CreateButton } from "./components/CreateButton";
 
-
-
 import { CreateEventScreen } from "./screens/CreateEventScreen";
 
 function App() {
-  const [showHome, setShowHome] = useState(false);
+  const [showHome, setShowHome] = useState(true);
   const [showCreateEvent, setShowCreateEvent] = useState(false);
   const [currCard, setCard] = useState(null);
-
 
   const toggleCreateEvent = () => {
     setShowCreateEvent((prev) => !prev);
@@ -49,16 +46,17 @@ function App() {
           <CreateButton handleClick={toggleCreateEvent} />
         </>
       )}
-      <SideBar rsoData={RSOs} 
-        showHome = {showHome}
-        setShowHome = {setShowHome}
-        currCard = {currCard}
-        setCard = {setCard}
-        showCreateEvent = {showCreateEvent}
-        setShowCreateEvent = {setShowCreateEvent}
-        toggleCreateEvent = {toggleCreateEvent}
+      <SideBar
+        rsoData={RSOs}
+        showHome={showHome}
+        setShowHome={setShowHome}
+        currCard={currCard}
+        setCard={setCard}
+        showCreateEvent={showCreateEvent}
+        setShowCreateEvent={setShowCreateEvent}
+        toggleCreateEvent={toggleCreateEvent}
       />
-      {showHome ? <Home /> : <OneRSO currCard = {currCard} setCard = {setCard}/>}
+      {showHome ? <Home /> : <OneRSO currCard={currCard} setCard={setCard} />}
     </div>
   );
 }
--- src/components/DayContainer.js
+++ src/components/DayContainer.js
@@ -4,20 +4,13 @@ import React from "react";
 import HomeCard from "./HomeCard/HomeCard";
 
 export const DayContainer = ({ day, cardData, id }) => {
-  const filteredCards = cardData.filter((card) => {
-    return (
-      card.date.isSameOrAfter(moment()) &&
-      card.date.isSameOrBefore(moment().add(7, "days")) &&
-      card.date.isoWeekday() === id
-    );
-  });
-  if (filteredCards.length === 0) {
+  if (cardData.length === 0) {
     return;
   }
   return (
-    <div class="day" style={{ marginBottom: "80px" }}>
-      {`${day} - ${moment().add(id, "days").format("MM/DD/YYYY")}`}
-      {filteredCards.map((card) => (
+    <div class="day" style={{ marginBottom: "0px" }}>
+      {`${day}`}
+      {cardData.map((card) => (
         <div style={{ marginBottom: "20px" }}>
           <HomeCard
             bg={card.bg}
