40fa0c9f03e19a6e6a4d58c7464904ca40139b61
Yusuf Elley <yelley@vl965-172-31-196-3.wireless.umass.edu>
2022-11-13 07:51:26 -0500

    working version

--- src/screens/OneRSO.js
+++ src/screens/OneRSO.js
@@ -3,7 +3,8 @@ import EventCard from "../components/EventCard";
 import testFlyer from "../assets/flyer2.png";
 import { ScrollingContainer } from "../components/ScrollingContainer";
 import "./OneRSO.css";
-import About from "../components/About/About"
+import About from "../components/About/About";
+import moment from "moment";
 
 function OneRSO(props) {
   const currCard = props.currCard;
@@ -13,66 +14,60 @@ function OneRSO(props) {
     {
       image: testFlyer,
       title: "Test Title",
-      time: "1:00 PM",
-      date: "12/1/22",
+      date: moment({ year: 2022, month: 10, day: 17, hour: 15, minute: 10 }),
       location: "ILC",
     },
     {
       image: testFlyer,
       title: "Test Title",
-      time: "1:00 PM",
-      date: "12/1/22",
+      date: moment({ year: 2022, month: 10, day: 17, hour: 15, minute: 10 }),
       location: "ILC",
     },
     {
       image: testFlyer,
       title: "Test Title",
-      time: "1:00 PM",
-      date: "12/1/22",
+      date: moment({ year: 2022, month: 10, day: 17, hour: 15, minute: 10 }),
       location: "ILC",
     },
     {
       image: testFlyer,
       title: "Test Title",
-      time: "1:00 PM",
-      date: "12/1/22",
+      date: moment({ year: 2022, month: 10, day: 17, hour: 15, minute: 10 }),
       location: "ILC",
     },
     {
       image: testFlyer,
       title: "Test Title",
-      time: "1:00 PM",
-      date: "12/1/22",
+      date: moment({ year: 2022, month: 10, day: 17, hour: 15, minute: 10 }),
       location: "ILC",
     },
     {
       image: testFlyer,
       title: "Test Title",
-      time: "1:00 PM",
-      date: "12/1/22",
-      location: "ILC"
-    },
-    {
-      image: testFlyer,
-      title: "Test Title",
-      time: "1:00 PM",
-      date: "12/1/22",
+      date: moment({ year: 2022, month: 10, day: 17, hour: 15, minute: 10 }),
       location: "ILC",
     },
     {
       image: testFlyer,
       title: "Test Title",
-      time: "1:00 PM",
-      date: "12/1/22",
+      date: moment({ year: 2022, month: 10, day: 17, hour: 15, minute: 10 }),
       location: "ILC",
     },
-
+    {
+      image: testFlyer,
+      title: "Test Title",
+      date: moment({ year: 2022, month: 10, day: 17, hour: 15, minute: 10 }),
+      location: "ILC",
+    },
   ];
   return (
     <div>
       <div className="one-RSO-header">
         <div className="club-name-header">MSA</div>
-        <About rsoName="MSA" body="hdskdjhwjasdljw s ldjwoiajdslkwja  daskldjdwj" />
+        <About
+          rsoName="MSA"
+          body="hdskdjhwjasdljw s ldjwoiajdslkwja  daskldjdwj"
+        />
       </div>
       <ScrollingContainer cardData={eventCards} />
     </div>
